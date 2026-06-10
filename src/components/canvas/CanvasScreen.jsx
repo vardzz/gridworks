20171aee962b5d7f1a40f98892581d4507fd5fd2
Jericho Varde <vardejericho@gmail.com>
2026-06-10 11:22:03 +0800

    feat: create CanvasScreen component to assemble schedule grid and style controls with mobile drawer support

--- src/components/canvas/CanvasScreen.jsx
+++ src/components/canvas/CanvasScreen.jsx
@@ -90,7 +90,7 @@ export default function CanvasScreen({
         <div className="overflow-x-auto max-lg:-mx-3 max-lg:px-3">
           <div
             ref={canvasRef}
-            className="w-full max-w-[1200px] mx-auto"
+            className="w-full"
             data-theme={prefs.theme_id || "minimalist_bureau"}
             style={{ ...canvasOverrides, fontFamily: prefs.font_family || "var(--gw-font-display)" }}
           >
